1e15a39afdbd71fa641107c9059fac75ab6a3397
ethan jones <etn.jones@gmail.com>
2023-03-04 15:14:27 -0600

    longer datasets.

--- src/toy_dataset.rs
+++ src/toy_dataset.rs
@@ -8,3 +8,25 @@ pub const TOY_VECTOR_REF_LEN4: [Nucleotide; 4] =
 
 pub const TOY_VECTOR_A_LEN4: [Nucleotide; 4] =
     [Nucleotide::A, Nucleotide::T, Nucleotide::C, Nucleotide::A];
+
+pub const TOY_VECTOR_REF_LEN8: [Nucleotide; 8] = [
+    Nucleotide::A,
+    Nucleotide::T,
+    Nucleotide::C,
+    Nucleotide::G,
+    Nucleotide::A,
+    Nucleotide::A,
+    Nucleotide::A,
+    Nucleotide::A,
+];
+
+pub const TOY_VECTOR_A_LEN8: [Nucleotide; 8] = [
+    Nucleotide::A,
+    Nucleotide::T,
+    Nucleotide::C,
+    Nucleotide::A,
+    Nucleotide::G,
+    Nucleotide::G,
+    Nucleotide::G,
+    Nucleotide::G,
+];
