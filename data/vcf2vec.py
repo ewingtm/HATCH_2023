96e36e0beef2d997538b8cb9433944bc7f4602dd
Phillip Ewing <phillip.ewing@perkinswiill.com>
2023-03-04 22:33:37 -0600

    Simiilarity matrix generator rough draft.

--- data/vcf2vec.py
+++ data/vcf2vec.py
@@ -2,12 +2,6 @@ import pandas as pd
 import re
 from enum import Enum
 
-class Nucleotide(Enum):
-    A = 1,
-    T = 2,
-    C = 3,
-    G = 4
-
 class VcfObject(object):
 
     NUCLEOTIDE = {
@@ -27,12 +21,10 @@ class VcfObject(object):
             idx_A, idx_B = (int(n) for n in re.split('[/|]', row[self.data.columns[-1]]))
             gt_A = self.NUCLEOTIDE[row["ALT"][idx_A - 1][0] if idx_A != 0 else row["REF"][0]]
             gt_B = self.NUCLEOTIDE[row["ALT"][idx_B - 1][0] if idx_B != 0 else row["REF"][0]]
-            print(gt_A, gt_B)
 
             # NOTE: This is just an arbitrary assumption about the nucleotide's
             # distance from the reference genome value; to be revisited in
             # future iterations!
-
             self.vector.append(max(gt_A, gt_B))
 
 if __name__ == "__main__":
