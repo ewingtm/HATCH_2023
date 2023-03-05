7734a6a0c45758e1727f5d9656ba89803f9ec40a
Phillip Ewing <phillip.ewing@perkinswiill.com>
2023-03-05 03:38:23 -0600

    Refactor visualization modules.

--- data/parse_vcf.py
+++ data/parse_vcf.py
@@ -1,14 +1,15 @@
 import pandas as pd
+import os
 
 if __name__ == "__main__":
 
-    F_IN = "ALL.chr14.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf"
+    F_IN = "./data/ALL.chr14.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf"
 
     for i in range(9, 109):
         sel_columns = list(range(9)) + [i]
         df = pd.read_table(F_IN, skiprows=19, nrows=10000, low_memory=False, usecols=sel_columns)
         filename = df.columns[-1] + "_10K.csv"
-        df.to_csv(filename, mode='a', index=False)
+        df.to_csv(os.path.join("./data/examples/", filename), mode='a', index=False)
 
         print("Writing...", filename)
     
--- data/vcf2vec.py
+++ data/vcf2vec.py
@@ -12,12 +12,13 @@ class VcfObject(object):
     }
 
     def __init__(self, filepath: str):
-        self.data = pd.read_csv(filepath)
+        self.data = pd.read_csv(filepath, header=0)
         self.vector = []
         self.to_vector()
 
     def to_vector(self):
-        for index, row in self.data.iterrows():
+        for _, row in self.data.iterrows():
+            print(row)
             idx_A, idx_B = (int(n) for n in re.split('[/|]', row[self.data.columns[-1]]))
             gt_A = self.NUCLEOTIDE[row["ALT"][idx_A - 1][0] if idx_A != 0 else row["REF"][0]]
             gt_B = self.NUCLEOTIDE[row["ALT"][idx_B - 1][0] if idx_B != 0 else row["REF"][0]]
@@ -27,7 +28,7 @@ class VcfObject(object):
             # future iterations!
             self.vector.append(max(gt_A, gt_B))
 
-if __name__ == "__main__":
-    FPATH = "./data/examples/HG00096_10K.csv"
-    vcf = VcfObject(FPATH)
-    print(vcf.vector)
+# if __name__ == "__main__":
+#     FPATH = "./data/examples/HG00096_10K.csv"
+#     vcf = VcfObject(FPATH)
+#     print(vcf.vector)
