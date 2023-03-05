c1e46482313e946ef7591f47116f0491290b214e
Phillip Ewing <phillip.ewing@perkinswiill.com>
2023-03-04 19:27:22 -0600

    Add converter for simplifying gene variant vector.

---
+++ data/vcf2vec.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import re
+from enum import Enum
+
+class Nucleotide(Enum):
+    A = 1,
+    T = 2,
+    C = 3,
+    G = 4
+
+class VcfObject(object):
+
+    NUCLEOTIDE = {
+        'A': 1,
+        'T': 2,
+        'C': 3,
+        'G': 4
+    }
+
+    def __init__(self, filepath: str):
+        self.data = pd.read_csv(filepath)
+        self.vector = []
+        self.to_vector()
+
+    def to_vector(self):
+        for index, row in self.data.iterrows():
+            idx_A, idx_B = (int(n) for n in re.split('[/|]', row[self.data.columns[-1]]))
+            gt_A = self.NUCLEOTIDE[row["ALT"][idx_A - 1][0] if idx_A != 0 else row["REF"][0]]
+            gt_B = self.NUCLEOTIDE[row["ALT"][idx_B - 1][0] if idx_B != 0 else row["REF"][0]]
+            print(gt_A, gt_B)
+
+            # NOTE: This is just an arbitrary assumption about the nucleotide's
+            # distance from the reference genome value; to be revisited in
+            # future iterations!
+
+            self.vector.append(max(gt_A, gt_B))
+
+if __name__ == "__main__":
+    FPATH = "./data/examples/HG00096_10K.csv"
+    vcf = VcfObject(FPATH)
+    print(vcf.vector)
